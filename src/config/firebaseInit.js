d4310b4850f3de5a4257d2747363057056896b30
Shinia Gupta <shiniavit@gmail.com>
2024-04-24 21:03:32 +0530

    modify config files

--- src/config/firebaseInit.js
+++ src/config/firebaseInit.js
@@ -8,12 +8,12 @@ import {getAuth,GoogleAuthProvider} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuk9fkRcZz6DnPm7CCJ1nQ47IXBf3Q-MQ",
-  authDomain: "busybuy-548f3.firebaseapp.com",
-  projectId: "busybuy-548f3",
-  storageBucket: "busybuy-548f3.appspot.com",
-  messagingSenderId: "1059018591312",
-  appId: "1:1059018591312:web:02b002f095cba515b29555"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
